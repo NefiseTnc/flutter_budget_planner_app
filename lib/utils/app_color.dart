df6224c2c03d4eb87a20100501d112d61207e1c4
Nefise Banu Tunç <100851004+NefiseTnc@users.noreply.github.com>
2024-09-22 04:41:04 +0300

    first commit

---
+++ lib/utils/app_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+
+  static const whiteColor = Colors.white;
+  static const blackColor = Colors.black;
+  static const pinkColor = Color(0xFFF35288);
+  static const greenColor = Color(0xFF40C900);
+  static const redColor = Color(0xFFC90C00);
+  static const greyColor = Color(0xFF91919F);
+}
